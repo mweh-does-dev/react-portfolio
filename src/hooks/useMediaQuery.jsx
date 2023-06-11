49c0f2074a591624064d21bc0efdacaf0d576e5f
mweh-does-dev <josefin.berntsson@chasacademy.se>
2023-06-11 11:54:56 +0200

    changes

--- src/hooks/useMediaQuery.jsx
+++ src/hooks/useMediaQuery.jsx
@@ -8,8 +8,10 @@ const useMediaQuery = (query) => {
     if (media.matches !== matches) {
       setMatches(media.matches);
     }
+    // Lyssna på ändringar i fönstrets storlek
     const listener = () => setMatches(media.matches);
     window.addEventListener("resize", listener);
+
     return () => window.removeEventListener("resize", listener);
   }, [matches, query]);
 
